219c06854a94a0ed65293d5d436af2a4d66125a4
Dmitry Kanaev <dk@sbctech.ru>
2019-12-16 08:52:29 +0300

    add Serializable example 1

---
+++ src/main/java/WriteObject.java
@@ -0,0 +1,22 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class WriteObject {
+    public static void main(String[] args) {
+        Person person1 = new Person(1, "Bob");
+        Person person2 = new Person(2, "Mike");
+
+        try {
+            FileOutputStream fos = new FileOutputStream("people.bin"); // класс для записи байтов
+            ObjectOutputStream oos = new ObjectOutputStream(fos); // класс для записи объектов
+
+            oos.writeObject(person1);
+            oos.writeObject(person2);
+
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
